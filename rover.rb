63fbec6dbe9c5624535cc8c4980ddd5f20462e9e
alexignat <d.alexandru.ignat@gmail.com>
2015-06-11 16:26:50 -0400

    Starting to create methods

--- rover.rb
+++ rover.rb
@@ -1,10 +1,45 @@
 # Exercise 2: Mars Rover
 
-
 class Rover
-  def initialize
+
+  attr_reader :x, :y, :direction
+
+  def initialize(x, y, direciton)
     @x = x
     @y = y
     @direction = direction
   end
+
+  def read_instruction
+
+  end
+
+  def turn_left
+  end
+
+  def turn_right
+  end
+
+  def move
+    if @direction == "N"
+      @y += 1
+    elsif @direction == "S"
+      @y -= 1
+    elsif @direction = "E"
+      @x += 1
+    elsif @direction = "W"
+      @x -= 1
+    end
+
+  end
+
+  def to_s
+    "Coordinates: #{@x}, #{@y}, Facing #{@direction}"
+  end
+
 end
+
+
+r = Rover.new(2, 2, "N")
+
+puts r.to_s
