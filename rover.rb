f2468845926ff18a657b488d5c5215576a747222
alexignat <d.alexandru.ignat@gmail.com>
2015-06-11 17:22:22 -0400

    Rover class is functional

--- rover.rb
+++ rover.rb
@@ -2,44 +2,72 @@
 
 class Rover
 
+  #We only need attr_reader b/c we we just need to read commands, not change them
   attr_reader :x, :y, :direction
 
-  def initialize(x, y, direciton)
+  def initialize(x, y, direction) # direction ="N"
     @x = x
     @y = y
-    @direction = direction
+    @compass = ["N", "E", "S", "W"]
+    @facing = @compass.index(direction)
   end
 
-  def read_instruction
-
-  end
-
-  def turn_left
+  def read_instruction(command)
+    if command = "R"
+      turn_right
+    elsif command = "L"
+      turn_left
+    elsif command = "M"
+      move
+    end
   end
 
   def turn_right
+    @compass = @compass.rotate
   end
 
-  def move
-    if @direction == "N"
+
+  def turn_left
+    @compass = @compass.rotate(-1)
+  end
+
+  # The rover will move in whatever direction it is facing
+  # The grid is a set of numbers, so if you want it to move
+  # up, down, left or right it will execute those instructions by incrementing the x and y coordinates
+
+def move
+    if @compass[@facing] == "N"
       @y += 1
-    elsif @direction == "S"
+    elsif @compass[@facing] == "S"
       @y -= 1
-    elsif @direction = "E"
+    elsif @compass[@facing] == "E"
       @x += 1
-    elsif @direction = "W"
+    elsif @compass[@facing]== "W"
       @x -= 1
     end
 
   end
 
   def to_s
-    "Coordinates: #{@x}, #{@y}, Facing #{@direction}"
+    "Coordinates: #{@x}, #{@y}, Facing #{@compass[@facing]}"
   end
 
 end
 
+# Figure out a way to get info and then pass the methods onto it
+
+
+
+
+# Create the rover class
 
 r = Rover.new(2, 2, "N")
 
+r.turn_right
+puts r.to_s
+
+r.turn_left
+puts r.to_s
+
+r.move
 puts r.to_s
