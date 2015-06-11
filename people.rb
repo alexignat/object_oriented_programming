807e299983fdf0b53f93cacc083cd9b9dac275df
alexignat <d.alexandru.ignat@gmail.com>
2015-06-11 14:15:17 -0400

    Created instance of instructor named 'Chris'

--- people.rb
+++ people.rb
@@ -6,16 +6,16 @@
 # Created a parent class called Person
 # This will apply to the Student class and Instructor class
 
-
 class Person
   def initialize(name)
     @name = name
   end
 
-  def say_hi(name)
-    puts "Hi, my name is #{name}"
+  def greeting
+    puts "Hi my name is #{@name}"
   end
 end
+
 
 class Student < Person
   def learn
@@ -23,8 +23,29 @@ class Student < Person
   end
 end
 
-class Instructor <Person
+class Instructor < Person
   def teach
-    "Everything in Ruby is an Object"
+    "Everything in Ruby is an Object."
   end
 end
+
+first_instructor = Instructor.new("Chris")
+first_instructor.greeting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
