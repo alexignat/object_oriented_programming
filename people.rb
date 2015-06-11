38667d00f7255146d5c400cfaabe9813643b9c12
alexignat <d.alexandru.ignat@gmail.com>
2015-06-11 13:57:48 -0400

    Added greeting method

--- people.rb
+++ people.rb
@@ -2,20 +2,28 @@
 # Ruby Object Oriented Programming
 # Exercise 1
 
+
+# Created a parent class called Person
+# This will apply to the Student class and Instructor class
+
+
 class Person
   def initialize(name)
     @name = name
   end
+
+  def say_hi(name)
+    puts "Hi, my name is #{name}"
+  end
 end
 
-
-class Student
+class Student < Person
   def learn
     "I get it!"
   end
 end
 
-class Instructor
+class Instructor <Person
   def teach
     "Everything in Ruby is an Object"
   end
