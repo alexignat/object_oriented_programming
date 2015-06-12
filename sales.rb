f65493c23ea3f6d833a04562267cfbc2236e5bf2
alexignat <d.alexandru.ignat@gmail.com>
2015-06-12 16:17:09 -0400

    Receipt class can take an item

--- sales.rb
+++ sales.rb
@@ -2,62 +2,97 @@
 # Assingment 4 Exercise 3
 
 
-# This class is tax exempt
-class TaxExempt
-  attr_accessor :item, :price
-
-  def initialize(item, price)
+class Receipt
+  # This add_items method will take items and add them to an array
+  def add_item(item)
     @item = item
-    @price = price
-  end
-
-  def tax
-    tax = (@price * 0)
   end
 end
 
-book = TaxExempt.new(book, 10)
-puts book.tax
+receipt = Receipt.new
 
-# RegularItem class has a tax amount of 10%
-# The class calculates the tax rate
-# Takes item and price as arguments
+apple = receipt.add_item("apple")
+puts apple
 
-class RegularItem
-  attr_accessor :item, :price
 
-  def initialize(item, price)
-    @item = item
-    @price = price
-  end
 
-  def tax
-    tax = (@price * 0.10).to_f
-  end
-end
 
-perfume = RegularItem.new(perfume, 10)
-puts perfume.tax
 
-# ImportedItem class has a tax amount of 15%
-# The class calculates the tax rate
-# Taks an item and price as arguments
 
-class ImportedItem
-  attr_accessor :item, :price
 
-  def initialize(item, price)
-    @item = item
-    @price = price
-  end
 
-  def tax
-    tax = (@price * 0.15).to_f
-  end
-end
 
-chocolate_bar = ImportedItem.new(chocolate_bar, 10)
-puts chocolate_bar.tax
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # This class is tax exempt
+# class TaxExempt
+#   attr_accessor :item, :price
+
+#   def initialize(item, price)
+#     @item = item
+#     @price = price
+#   end
+
+#   def tax
+#     tax = (@price * 0)
+#   end
+# end
+
+# book = TaxExempt.new(book, 10)
+# puts book.tax
+
+# # RegularItem class has a tax amount of 10%
+# # The class calculates the tax rate
+# # Takes item and price as arguments
+
+# class RegularItem
+#   attr_accessor :item, :price
+
+#   def initialize(item, price)
+#     @item = item
+#     @price = price
+#   end
+
+#   def tax
+#     tax = (@price * 0.10).to_f
+#   end
+# end
+
+# perfume = RegularItem.new(perfume, 10)
+# puts perfume.tax
+
+# # ImportedItem class has a tax amount of 15%
+# # The class calculates the tax rate
+# # Taks an item and price as arguments
+
+# class ImportedItem
+#   attr_accessor :item, :price
+
+#   def initialize(item, price)
+#     @item = item
+#     @price = price
+#   end
+
+#   def tax
+#     tax = (@price * 0.15).to_f
+#   end
+# end
+
+# chocolate_bar = ImportedItem.new(chocolate_bar, 10)
+# puts chocolate_bar.tax
 
 
 
